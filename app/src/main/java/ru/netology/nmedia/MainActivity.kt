b1202a7e70412b1ddb47b411c9023d218790b19b
Timur Zaripov <zaripov_t99@mail.ru>
2021-04-01 11:55:35 +0700

    homeWork2.1.v1

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,90 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Меняем карьеру через образование",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу.\n" +
+                    "         Затем появились курсы по дизайну, разработке, аналитике и управлению.\n" +
+                    "         Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов.\n" +
+                    "         Мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее.\n" +
+                    "         Наша миссия - помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "24 марта в 23:23",
+            likedByMe = false
+        )
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            if (post.likedByMe) {
+                like?.setImageResource(R.drawable.ic_liked_24)
+            }
+            countLike?.text = post.likes.toString()
+            countShare?.text = post.shares.toString()
+
+            like?.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                like.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like
+                )
+
+                if (post.likedByMe) {
+                    post.likes++
+                    countLike?.text = formatOut(post.likes)
+                } else {
+                    post.likes--
+                    countLike?.text = formatOut(post.likes)
+                }
+            }
+
+            share?.setOnClickListener{
+                post.shares++
+                countShare?.text = formatOut(post.shares)
+            }
+        }
+    }
+
+    private fun formatOut(count: Int): String {
+        return when (count) {
+            in 0..999 -> {
+                return count.toString()
+            }
+            in 1000..1099 -> {
+                return (count / 1000).toString() + "K"
+            }
+            in 1100..9999 -> {
+                return "%.1f".format(count / 1000.0).replace(",", ".") + "K"
+            }
+            in 10000..10099 -> {
+                return (count / 1000).toString() + "K"
+            }
+            in 10100..99999 -> {
+                return "%.1f".format(count / 10000.0).replace(",", ".") + "K"
+            }
+            in 100000..100999 -> {
+                return (count / 100000).toString() + "K"
+            }
+            in 110000..999999 -> {
+                return "%.1f".format(count / 100000.0).replace(",", ".") + "K"
+            }
+            in 1000000..1099999 -> {
+                return (count / 1000000).toString() + "M"
+            }
+            in 1100000..9999999 -> {
+                return "%.1f".format(count / 1000000.0).replace(",", ".") + "M"
+            }
+            else -> "error"
+        }
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia
+
+data class Post (
+        val id: Long,
+        val author: String,
+        val content: String,
+        val published: String,
+        var likes: Int = 999999,
+        var shares: Int = 999999,
+        var likedByMe: Boolean
+)
